af1c0472d6c74c2185a5bf91ed9258047b5b3e40
Danielle Meijler <daniellemeijler@gmail.com>
2025-09-18 20:59:11 +0300

    created basic ippacket class

--- icmtcp/packet/ip_packet.py
+++ icmtcp/packet/ip_packet.py
@@ -1,16 +1,26 @@
 from scapy.all import IP, Raw
+import socket
 
 class IPPacket(object):
-    def __init__(self, src_ip: str, dst_ip: str, payload: str):
-        self._src_ip = src_ip
-        self._dst_ip = dst_ip
-        self._payload = payload
+    def __init__(self):
+        self.src_ip = ''
+        self.dst_ip = ''
+        self.payload = ''
+
+    def set(self, src_ip, dst_ip, payload):
+        self.src_ip = src_ip
+        self.dst_ip = dst_ip
+        self.payload = payload
 
     def compile(self) -> bytes:
-        packet = IP(src=self._src_ip,
-                    dst=self._dst_ip) / Raw(load=self._payload)
+        packet = Raw(load=self.payload)
         return bytes(packet)
     
-    def decompile(self, raw_bytes: bytes) -> None:
+    def decompile(self, raw_bytes: bytes):
         packet = IP(raw_bytes)
-        print(packet.show())
+        self.src_ip = packet.getlayer('IP').src
+        self.dst_ip = packet.getlayer('IP').dst
+        self.payload = packet.getlayer('Raw').load
+
+    def send(self, sock: socket.socket) -> None:
+        sock.sendto(self.compile(), (self.dst_ip, 0))
