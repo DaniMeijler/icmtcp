95d0a039a073f1076204de7769d21b492f761dcb
Danielle Meijler <daniellemeijler@gmail.com>
2025-09-19 16:57:30 +0300

    fixed icmp and ip packet classes

---
+++ icmtcp/packet/icmp_packet.py
@@ -0,0 +1,32 @@
+from scapy.all import ICMP, Raw, Packet
+from .ip_packet import IPPacket
+
+class ICMPPacket(IPPacket):
+    def __init__(self):
+        self.code = 0
+        self.type = 0
+        self.seq = 0
+        self.id = 0
+        self.checksum = 0
+        self.icmp_layer = ICMP(type=0, code=0, seq=0, id=0)
+
+    def get_icmp_layer(self) -> ICMP:
+        return ICMP(type=self.type, code=self.code, seq=self.seq, id=self.id)
+    
+    def get_packet(self) -> Packet:
+        return self.get_ip_layer() / self.get_icmp_layer() / Raw(load=self.payload)
+
+    def compile(self) -> bytes:
+        icmp_layer = self.get_icmp_layer()
+        data = Raw(load=self.payload)
+        return bytes(icmp_layer / data)
+    
+    def decompile(self, raw_bytes: bytes) -> None:
+        super().decompile(raw_bytes)
+        icmp = ICMP(self.payload)
+        self.code = icmp.code
+        self.type = icmp.type
+        self.id = icmp.id
+        self.seq = icmp.seq
+        self.payload = icmp.payload
+        
--- icmtcp/packet/ip_packet.py
+++ icmtcp/packet/ip_packet.py
@@ -1,13 +1,22 @@
-from scapy.all import IP, Raw
+from scapy.all import IP, Raw, Packet
 import socket
 
 class IPPacket(object):
     def __init__(self):
-        self.src_ip = ''
+        self.src_ip = '0.0.0.0'
         self.dst_ip = ''
         self.payload = ''
 
-    def set(self, src_ip, dst_ip, payload):
+    def get_ip_layer(self) -> IP:
+        return IP(src=self.src_ip, dst=self.dst_ip)
+    
+    def get_packet(self) -> Packet:
+        return self.get_ip_layer() / Raw(load=self.payload)
+    
+    def __repr__(self) -> str:
+        return self.get_packet().__repr__()
+
+    def set(self, src_ip: str, dst_ip: str, payload: str) -> None:
         self.src_ip = src_ip
         self.dst_ip = dst_ip
         self.payload = payload
@@ -16,11 +25,9 @@ class IPPacket(object):
         packet = Raw(load=self.payload)
         return bytes(packet)
     
-    def decompile(self, raw_bytes: bytes):
-        packet = IP(raw_bytes)
-        self.src_ip = packet.getlayer('IP').src
-        self.dst_ip = packet.getlayer('IP').dst
-        self.payload = packet.getlayer('Raw').load
+    def decompile(self, raw_bytes: bytes) -> None:
+        ip = IP(raw_bytes)
+        self.src_ip = ip.getlayer('IP').src
+        self.dst_ip = ip.getlayer('IP').dst
+        self.payload = ip.getlayer('Raw').load
 
-    def send(self, sock: socket.socket) -> None:
-        sock.sendto(self.compile(), (self.dst_ip, 0))
